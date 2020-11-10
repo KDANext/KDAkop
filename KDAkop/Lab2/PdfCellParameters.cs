5b2ea9aaf9584aa7bcb6481919f166afca155a5c
KDANext <dima.1998.gig@mail.ru>
2020-11-10 22:35:26 +0400

    Lab1 3/3 Lab 2 1/3

---
+++ KDAkop/Lab2/PdfCellParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDAkop.Lab2
+{
+    class PdfCellParameters
+    {
+        public Cell Cell { get; set; }
+        public string Text { get; set; }
+        public string Style { get; set; }
+        public ParagraphAlignment ParagraphAlignment { get; set; }
+        public Unit BorderWidth { get; set; }
+    }
+}
